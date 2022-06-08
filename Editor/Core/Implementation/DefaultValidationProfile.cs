5ac91cbb69f92abbf566b8f8d19bd2db5cdbcf13
Paul FIGIEL <pf@homagames.com>
2022-06-08 15:41:53 +0200

    Better description for default profile

--- Editor/Core/Implementation/DefaultValidationProfile.cs
+++ Editor/Core/Implementation/DefaultValidationProfile.cs
@@ -9,7 +9,12 @@ namespace HomaGames.GameDoctor.Core
     {
         public const string DefaultValidationProfileName = "Default Validation Profile";
         public string Name => DefaultValidationProfileName;
-        public string Description => "Default Validation Profile with all available checks.";
+
+        public string Description =>
+            "Default Validation Profile with all available checks.\n\n" +
+            "Checkout Project Settings > Game Doctor and create new Profile from presets.\n" +
+            "Implement checks by inheriting ICheck and registering them with AvailableChecks.RegisterCheck.";
+
         public HashSet<ICheck> CheckList => new HashSet<ICheck>(AvailableChecks.GetAllChecks());
     }
 }
