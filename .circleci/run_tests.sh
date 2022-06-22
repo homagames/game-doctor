c094c3b641cfd370a6f052b664d876dbe0ce9363
Paul FIGIEL <pf@homagames.com>
2022-06-22 15:44:26 +0200

    Using generic Unity path

--- .circleci/run_tests.sh
+++ .circleci/run_tests.sh
@@ -2,21 +2,13 @@
 
 set -e 
 
-if [ "$CI" == "true" ]; then
-	echo "Using CI"
-	UNITY_BIN="/opt/Unity/Editor/Unity"
-else
-	echo "Using local Unity"
-	UNITY_BIN="/Applications/Unity/Hub/Editor/2020.3.21f1/Unity.app/Contents/MacOS/Unity"
-fi
-
 TEST_RESULTS_FILE="Build/Game Doctor/tests.xml"
 mkdir -p "Build/Game Doctor/"
 
 # GENERIC TESTS
 echo "Executing Generic Tests..."
 
-$UNITY_BIN -runTests -projectPath . -runSynchronously -nographics -batchmode -testPlatform "EditMode" -buildTarget "Android" -testResults "Build/Game Doctor/tests.xml" -nolog || true
+unity-editor -runTests -projectPath . -runSynchronously -nographics -batchmode -testPlatform "EditMode" -buildTarget "Android" -testResults "Build/Game Doctor/tests.xml" -nolog || true
 
 TESTS_COUNT=$(grep 'HomaGames.GameDoctor.Tests.dll' "$TEST_RESULTS_FILE" | grep -o 'testcasecount=\"[0-9][0-9]*\"' | grep -o '[0-9][0-9]*')
 PASSED_TESTS=$(grep 'HomaGames.GameDoctor.Tests.dll' "$TEST_RESULTS_FILE" | grep -o 'passed=\"[0-9][0-9]*\"' | grep -o '[0-9][0-9]*')
@@ -32,7 +24,7 @@ fi
 # iOS TESTS
 echo "Executing iOS Tests..."
 
-$UNITY_BIN -runTests -projectPath . -runSynchronously -nographics -batchmode -testPlatform "EditMode" -buildTarget "iOS" -testResults "Build/Game Doctor/tests.xml" -nolog || true
+unity-editor -runTests -projectPath . -runSynchronously -nographics -batchmode -testPlatform "EditMode" -buildTarget "iOS" -testResults "Build/Game Doctor/tests.xml" -nolog || true
 
 TESTS_COUNT=$(grep 'HomaGames.GameDoctor.Tests.dll' "$TEST_RESULTS_FILE" | grep -o 'testcasecount=\"[0-9][0-9]*\"' | grep -o '[0-9][0-9]*')
 PASSED_TESTS=$(grep 'HomaGames.GameDoctor.Tests.dll' "$TEST_RESULTS_FILE" | grep -o 'passed=\"[0-9][0-9]*\"' | grep -o '[0-9][0-9]*')
