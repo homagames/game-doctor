5660fcdc04899225e83cb8cbcd03ad9822fc00de
Clément <clement.couture@homagames.com>
2022-07-21 15:06:47 +0200

    Moved hash function to its rightful place

--- Editor/Implementation/IssueExtension.cs
+++ Editor/Implementation/IssueExtension.cs
@@ -1,3 +1,5 @@
+using JetBrains.Annotations;
+
 namespace HomaGames.GameDoctor.Core
 {
     public static class IssueExtension
@@ -10,6 +12,18 @@ namespace HomaGames.GameDoctor.Core
         {
             return issue.Name == otherIssue.Name && issue.Description == otherIssue.Description
                                                  && issue.GetType() == otherIssue.GetType();
+        }
+        
+        public static int GetHash([NotNull] this IIssue issue)
+        {
+            unchecked
+            {
+                var hashCode = (issue.Name != null ? issue.Name.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (issue.Description != null ? issue.Description.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (int) issue.AutomationType;
+                hashCode = (hashCode * 397) ^ (int) issue.Priority;
+                return hashCode;
+            }
         }
     }
 }
